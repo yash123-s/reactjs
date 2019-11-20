2717076fbee0144bd75d1192058e71d74dbc94ed
Yashaswini <ysureshostb2@gmail.com>
2019-11-20 17:31:56 +0530

    fpush

--- task1/ftask/src/App.js
+++ task1/ftask/src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Signin from './Components/signin/Signin';
-import Signup from './Components/signup/signup';
+// import Signin from './Components/signin/Signin';
+// import Signup from './Components/signup/signup';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Lifecycle from './Components/Lifecycle/Lifecycle';
+
 
 function App() {
   return (
     <div className="App">
-     <div>User Form Login Widget</div>
+     {/* <div>User Form Login Widget</div> */}
      <Router>
       <switch>
-        <Route exact path='/' component={Signup}></Route> 
-        <Route exact path='/Signin' component={Signin}></Route>
-       
-        {/* <Route exact path='/Contact' component={Contact}></Route>  */}
-        {/* <Route exact path='/Index' component={Index}></Route> */}
+        {/* <Route exact path='/' component={Signup}></Route>  */}
+        {/* <Route exact path='/Signin' component={Signin}></Route> */}
+        <Route exact path='/' component={Lifecycle}></Route>
       </switch>
       </Router>
     </div>
---
+++ task1/ftask/src/Components/Lifecycle/Lifecycle.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Lifecycle extends Component {
+        constructor(props){
+            super(props);
+            console.log('hey all');
+        }
+        componentWillMount(){
+            console.log('hiiii');
+        }
+        render() {
+            return (
+                <div>
+                    hello erveryone
+                </div>
+            );
+        }
+        componentDidMount(){
+            console.log('bye');
+        }
+    }
+
+
+export default Lifecycle;
