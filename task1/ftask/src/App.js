30a3ebdd0130432a0eb0bcd837781e863c460cba
Yashaswini <ysureshostb2@gmail.com>
2019-11-21 17:46:14 +0530

    five

--- task1/ftask/src/App.js
+++ task1/ftask/src/App.js
@@ -1,25 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , {Component} from 'react';
+// import logo from './logo.svg';
 import './App.css';
 // import Signin from './Components/signin/Signin';
 // import Signup from './Components/signup/signup';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Lifecycle from './Components/Lifecycle/Lifecycle';
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
+import UserInput from './Components/UserInput/UserInput';
+import UserOutput from './Components/UserOutput/UserOutput';
+// import Lifecycle from './Components/Lifecycle/Lifecycle';
 
 
-function App() {
-  return (
+
+class App extends Component {
+  state ={
+    person:[{
+      name:"tsg", 
+      age: '20'
+    }]
+  }
+  buttonClickHandler=()=>{
+    console.log('hi');
+    this.setState({
+      person:[{
+        name:"gala",
+        age:'21'
+      }]
+    })
+}
+  render() {
+      return (
+// function App() {
+//   return (
     <div className="App">
-     {/* <div>User Form Login Widget</div> */}
-     <Router>
-      <switch>
-        {/* <Route exact path='/' component={Signup}></Route>  */}
-        {/* <Route exact path='/Signin' component={Signin}></Route> */}
-        <Route exact path='/' component={Lifecycle}></Route>
-      </switch>
-      </Router>
+     {/* {/ <div>User Form Login Widget</div> /}
+     {/ <Router> /}
+      {/ <switch> /}
+        {/ <Route exact path='/' component={Signup}></Route>  /}
+        {/ <Route exact path='/Signin' component={Signin}></Route> /}
+        {/ <Route exact path='/' component={Lifecycle}></Route> /}
+      {/ </switch> /}
+      {/ </Router> /} */}
+      <button onClick={this.buttonClickHandler}>click me</button>
+     
+      <UserOutput>I am {this.state.person[0].name}</UserOutput>
+      <UserOutput Username="YSG"></UserOutput>
+     
     </div>
   );
 }
-
+}
 export default App;
---
+++ task1/ftask/src/Components/UserInput/UserInput.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+
+class UserInput extends Component {
+    render() {
+        return (
+            <div>
+              <input type="text" /> 
+            </div>
+        );
+    }
+}
+
+export default UserInput;
