e5aee7588fb525de62e3a35fb0ac0e81e7087b53
Yashaswini <ysureshostb2@gmail.com>
2019-11-22 17:28:26 +0530

    friday

---
+++ task2/task5/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+// import Component from './Components/Component';
+import Task from './Components/Task';
+
+function App() {
+  return (
+    <div className="App">
+     <Task />
+    </div>
+  );
+}
+
+export default App;
